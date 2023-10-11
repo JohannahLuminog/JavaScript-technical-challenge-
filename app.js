14e02f72a90ce249b995669b019515759a8aae84
Johannah <johannah_9@hotmail.co.uk>
2023-10-11 23:27:42 +0100

    Commit changes

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-const express = require('express')
-const path = require('path')
-const stocks = require('./stocks')
+const express = require('express');
+const path = require('path');
+const stocks = require('./stocks');
 
-const app = express()
-app.use(express.static(path.join(__dirname, 'static')))
+const app = express();
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/stocks', async (req, res) => {
-  const stockSymbols = await stocks.getStocks()
-  res.send({ stockSymbols })
-})
+  try {
+    const stockSymbols = await stocks.getStocks();
+    res.send({ stockSymbols });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to retrieve stock data.' });
+  }
+});
 
 app.get('/stocks/:symbol', async (req, res) => {
-  const { params: { symbol } } = req
-  const data = await stocks.getStockPoints(symbol, new Date())
-  res.send(data)
-})
+  const { params: { symbol } } = req;
+  try {
+    const data = await stocks.getStockPoints(symbol, new Date());
+    res.send(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to retrieve stock data.' });
+  }
+});
 
-app.listen(3000, () => console.log('Server is running!'))
+app.listen(3000, () => console.log('Server is running!'));
--- static/scripts.js
+++ static/scripts.js
@@ -1,24 +1,73 @@
-const canvas = document.getElementById('chart')
-const ctx = canvas.getContext('2d')
+const canvas = document.getElementById('chart');
+const ctx = canvas.getContext('2d');
 
-function drawLine (start, end, style) {
-  ctx.beginPath()
-  ctx.strokeStyle = style || 'black'
-  ctx.moveTo(...start)
-  ctx.lineTo(...end)
-  ctx.stroke()
+function drawLine(start, end, style) {
+  ctx.beginPath();
+  ctx.strokeStyle = style || 'black';
+  ctx.moveTo(...start);
+  ctx.lineTo(...end);
+  ctx.stroke();
 }
 
-function drawTriangle (apex1, apex2, apex3) {
-  ctx.beginPath()
-  ctx.moveTo(...apex1)
-  ctx.lineTo(...apex2)
-  ctx.lineTo(...apex3)
-  ctx.fill()
+function drawTriangle(apex1, apex2, apex3) {
+  ctx.beginPath();
+  ctx.moveTo(...apex1);
+  ctx.lineTo(...apex2);
+  ctx.lineTo(...apex3);
+  ctx.fill();
 }
 
-drawLine([50, 50], [50, 550])
-drawTriangle([35, 50], [65, 50], [50, 35])
+drawLine([50, 50], [50, 550]);
+drawTriangle([35, 50], [65, 50], [50, 35]);
 
-drawLine([50, 550], [950, 550])
-drawTriangle([950, 535], [950, 565], [965, 550])
+drawLine([50, 550], [950, 550]);
+drawTriangle([950, 535], [950, 565], [965, 550]);
+
+// Define an empty array to hold the stock symbols
+let stockSymbols = [];
+
+// Code to query the backend for the list of available stocks and data for each stock
+document.addEventListener('DOMContentLoaded', function () {
+  // Make the spinner visible
+  const spinner = document.getElementById('spinner');
+  spinner.style.display = 'block';
+
+   fetch('/stocks')
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch stock data');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      stockSymbols = data.stockSymbols;
+      console.log('Available Stocks:', stockSymbols);
+
+      // Loop through the available stock symbols and fetch data for each
+      stockSymbols.forEach((symbol) => {
+        fetch(`/stocks/${symbol}`)
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error(`Failed to fetch data for ${symbol}`);
+            }
+            return response.json();
+          })
+          .then((stockData) => {
+            console.log(`Data for ${symbol}:`);
+            console.table(stockData); // Use console.table to display data in a structured/tabular way
+
+            // You can now do something with the data for each stock
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+          });
+      });
+
+      // Hide the spinner after all data is loaded
+      spinner.style.display = 'none';
+      console.log('Spinner is now hidden');
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+});
